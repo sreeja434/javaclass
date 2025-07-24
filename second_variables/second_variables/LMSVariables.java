37cb94c2d78eeca2fe3581e0424f8bc0c78c751a
sreeja434 <sirichowdary@gmail.com>
2025-07-25 00:05:18 +0530

    EMI

--- second_variables/second_variables/LMSVariables.java
+++ second_variables/second_variables/LMSVariables.java
@@ -6,12 +6,12 @@ public class LMSVariables {
         // Student → ID, Name, Email, Password, Gender, Mobile Number etc 
         // define datatypes
         short studentID = 101;
-        String studentName = "Ravi";
-        String studentEmail = "ravi@gmail.com";
-        String studentPassword = "pass@123$%^";
+        String studentName = "siri";
+        String studentEmail = "sirichowdary434@gmail.com";
+        String studentPassword = "Chowdary@434";
         char studentGender_1 = 'F';
         boolean isStudentMale = true;
-        long studentMobileNumber = 9000090;
+        long studentMobileNumber = 9347096798;
 
         // Car mahindra ;
 
